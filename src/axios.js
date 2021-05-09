c68384ec3ed3776ccf3f1f3e6c5339642424d8c7
aditya <adityalipu78@gmail.com>
2021-05-09 23:42:27 +0530

    initial commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : "http://localhost:9000",
+    baseURL : "https://whatsapp-mern-backnd.herokuapp.com/",
 })
 
 export default instance;
